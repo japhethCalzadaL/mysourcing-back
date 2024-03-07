2b963d8b8fc2593255a6fc4e2462f029b3256e04
japhethCalzadaL <japhethoz@gmail.com>
2024-03-07 16:41:27 -0600

    database

--- migrations/Version20240307191230.php
+++ migrations/Version20240307191230.php
@@ -20,7 +20,20 @@ final class Version20240307191230 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(125) NOT NULL, last_name VARCHAR(125) NOT NULL, last_second_name VARCHAR(125) NOT NULL, email VARCHAR(125) NOT NULL, phone VARCHAR(10) NOT NULL, postal_code VARCHAR(5) NOT NULL, state VARCHAR(125) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE DATABASE IF NOT EXISTS mysourcing');
+        $this->addSql('USE mysourcing'); 
+        $this->addSql('
+        CREATE TABLE users (
+            id INT AUTO_INCREMENT NOT NULL,
+            name VARCHAR(125) NOT NULL,
+            last_name VARCHAR(125) NOT NULL,
+            last_second_name VARCHAR(125) NOT NULL,
+            email VARCHAR(125) NOT NULL,
+            phone VARCHAR(10) NOT NULL,
+            postal_code VARCHAR(5) NOT NULL,
+            state VARCHAR(125) NOT NULL,
+            PRIMARY KEY(id)
+        ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
